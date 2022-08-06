cd143d8fbc1d2e0f22126f24748515d32795b547
adarshrajput845 <77545819+adarshrajput845@users.noreply.github.com>
2022-08-06 17:20:03 +0530

    Delete import math.py

--- import math.py
+++
@@ -1,9 +0,0 @@
-import math
-def calcSIN (x,y) :
-    k = math.sin(x)*math.cos(y) + math.cos(x)*math.sin(y)
-    return k
-    
-x = int(input("enter an angle"))
-y = int(input("Enter another angle"))
-m = calcSIN (x,y)
-print(m)
